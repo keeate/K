7770ddcc39a0eef1be79a0ee1a5f61af8ffd177a
keeate <90776775+keeate@users.noreply.github.com>
2023-09-05 00:13:08 +0900

    Add files via upload

---
+++ ishii.js
@@ -0,0 +1,241 @@
+const btn1 = document.getElementById('popupBtn1');
+const modal1 = document.getElementById('modalWrap1');
+const closeBtn1 = document.getElementById('closeBtn1');
+
+btn1.onclick = function() {
+  modal1.style.display = 'block';
+}
+closeBtn1.onclick = function() {
+  modal1.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+const btn2 = document.getElementById('popupBtn2');
+const modal2 = document.getElementById('modalWrap2');
+const closeBtn2 = document.getElementById('closeBtn2');
+
+btn2.onclick = function() {
+  modal2.style.display = 'block';
+}
+closeBtn2.onclick = function() {
+  modal2.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+const btn3 = document.getElementById('popupBtn3');
+const modal3 = document.getElementById('modalWrap3');
+const closeBtn3 = document.getElementById('closeBtn3');
+
+btn3.onclick = function() {
+  modal2.style.display = 'block';
+}
+closeBtn3.onclick = function() {
+  modal2.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+}
+const btn4 = document.getElementById('popupBtn4');
+const modal4 = document.getElementById('modalWrap4');
+const closeBtn4 = document.getElementById('closeBtn4');
+
+btn4.onclick = function() {
+  modal2.style.display = 'block';
+}
+closeBtn4.onclick = function() {
+  modal2.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
+const btn5 = document.getElementById('popupBtn5');
+const modal5 = document.getElementById('modalWrap5');
+const closeBtn5 = document.getElementById('closeBtn5');
+
+btn5.onclick = function() {
+  modal2.style.display = 'block';
+}
+closeBtn5.onclick = function() {
+  modal2.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal5) {
+    modal5.style.display = "none";
+  }
+}
+const btn6 = document.getElementById('popupBtn6');
+const modal6 = document.getElementById('modalWrap6');
+const closeBtn6 = document.getElementById('closeBtn6');
+
+btn6.onclick = function() {
+  modal6.style.display = 'block';
+}
+closeBtn6.onclick = function() {
+  modal6.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal6) {
+    modal6.style.display = "none";
+  }
+}
+const btn7 = document.getElementById('popupBtn7');
+const modal7 = document.getElementById('modalWrap7');
+const closeBtn7 = document.getElementById('closeBtn7');
+
+btn7.onclick = function() {
+  modal7.style.display = 'block';
+}
+closeBtn7.onclick = function() {
+  modal7.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal7) {
+    modal7.style.display = "none";
+  }
+}
+const btn8 = document.getElementById('popupBtn8');
+const modal8 = document.getElementById('modalWrap8');
+const closeBtn8 = document.getElementById('closeBtn8');
+
+btn8.onclick = function() {
+  modal8.style.display = 'block';
+}
+closeBtn8.onclick = function() {
+  modal8.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal8) {
+    modal8.style.display = "none";
+  }
+}
+const btn9 = document.getElementById('popupBtn9');
+const modal9 = document.getElementById('modalWrap9');
+const closeBtn9 = document.getElementById('closeBtn9');
+
+btn9.onclick = function() {
+  modal9.style.display = 'block';
+}
+closeBtn9.onclick = function() {
+  modal9.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal9) {
+    modal9.style.display = "none";
+  }
+}
+const btn10 = document.getElementById('popupBtn10');
+const modal10 = document.getElementById('modalWrap10');
+const closeBtn10 = document.getElementById('closeBtn10');
+
+btn10.onclick = function() {
+  modal10.style.display = 'block';
+}
+closeBtn10.onclick = function() {
+  modal10.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal10) {
+    modal10.style.display = "none";
+  }
+}
+
+const btn11 = document.getElementById('popupBtn11');
+const modal11 = document.getElementById('modalWrap11');
+const closeBtn11 = document.getElementById('closeBtn11');
+
+btn11.onclick = function() {
+  modal11.style.display = 'block';
+}
+closeBtn11.onclick = function() {
+  modal11.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal11) {
+    modal11.style.display = "none";
+  }
+}
+const btn12 = document.getElementById('popupBtn12');
+const modal12 = document.getElementById('modalWrap12');
+const closeBtn12 = document.getElementById('closeBtn12');
+
+btn12.onclick = function() {
+  modal12.style.display = 'block';
+}
+closeBtn12.onclick = function() {
+  modal12.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal12) {
+    modal12.style.display = "none";
+  }
+}
+const btn13 = document.getElementById('popupBtn13');
+const modal13 = document.getElementById('modalWrap13');
+const closeBtn13 = document.getElementById('closeBtn13');
+
+btn13.onclick = function() {
+  modal13.style.display = 'block';
+}
+closeBtn13.onclick = function() {
+  modal13.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal13) {
+    modal13.style.display = "none";
+  }
+}
+const btn14 = document.getElementById('popupBtn14');
+const modal14 = document.getElementById('modalWrap14');
+const closeBtn14 = document.getElementById('closeBtn14');
+
+btn14.onclick = function() {
+  modal14.style.display = 'block';
+}
+closeBtn14.onclick = function() {
+  modal14.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal14) {
+    modal14.style.display = "none";
+  }
+}
+const btn15 = document.getElementById('popupBtn15');
+const modal15 = document.getElementById('modalWrap15');
+const closeBtn15 = document.getElementById('closeBtn15');
+
+btn15.onclick = function() {
+  modal15.style.display = 'block';
+}
+closeBtn15.onclick = function() {
+  modal15.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal15) {
+    modal15.style.display = "none";
+  }
+}
